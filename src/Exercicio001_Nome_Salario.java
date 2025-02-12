cded239251693006bc8bdf235970ee750afc4555
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 20:41:57 -0300

    Alteração no nome

--- src/Exercicio001_Nome_Salario.java
+++ src/Exercicio001_Nome_Salario.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Nome_Salario {
+public class Exercicio001_Nome_Salario {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Qual é o seu nome?: ");
