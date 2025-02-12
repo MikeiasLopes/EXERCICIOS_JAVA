fc0fe195d8fa0933cd5e3ae5aefd3025dd622f3a
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 20:38:22 -0300

    Exercicio003

---
+++ src/Nome_Salario.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Nome_Salario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Qual é o seu nome?: ");
+        String nome = sc.nextLine();
+        System.out.print("Salário: ");
+        String salario = sc.nextLine();
+
+        System.out.print("O funcionário(a) " +nome+ " tem um salário de " +salario+ " em junho");
+    }
+}
