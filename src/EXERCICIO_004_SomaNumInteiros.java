f380750efb2c5fed2b98c7438459f81a80ec30f0
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 21:01:04 -0300

    Soma de dois números inteiros

--- src/EXERCICIO_004_SomaNumInteiros.java
+++ src/EXERCICIO_004_SomaNumInteiros.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class EXERCICIO_004_SomaNumInteiros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int num1 = sc.nextInt();
+        System.out.print("Digite um número inteiro: ");
+        int num2 = sc.nextInt();
+        int soma = num1 + num2;
+        System.out.println("Soma: " + soma);
     }
 }
