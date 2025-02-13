a0e7d1f5f9212036c699aa736ec0f5025966aa09
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 22:44:03 -0300

    Atualização

--- src/EXERCICIO_003_PrintName.java
+++ src/EXERCICIO_003_PrintName.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
 public class EXERCICIO_003_PrintName {
     public static void main(String[] args) {
+
+        //Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Digite seu nome: ");
         String nome = sc.nextLine();
+
         System.out.print("Olá " +nome+ ", é um pazer de conhecer!");
     }
 }
