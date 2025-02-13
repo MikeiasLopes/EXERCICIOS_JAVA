da5498e899c3cca94a49c44e50cc497955bdef26
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 22:49:55 -0300

    Atualização

--- src/EXERCICIO_007_DobroTercaParte.java
+++ src/EXERCICIO_007_DobroTercaParte.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class EXERCICIO_007_DobroTercaParte {
     public static void main(String[] args) {
+
+        //Crie um programa em Java que leia um número real e mostre na tela o seu dobro e a sua terça parte.
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Digite um numero: ");
         float numero = sc.nextFloat();
+
         float dobro = numero * 2;
         float TercaParte = numero / 3;
-        System.out.print("O dobro de " +numero+ " é: " +dobro+ "\nA terça parte de " +numero+ " é: " +TercaParte);
 
+        System.out.print("O dobro de " +numero+ " é: " +dobro+ "\nA terça parte de " +numero+ " é: " +TercaParte);
     }
 }
