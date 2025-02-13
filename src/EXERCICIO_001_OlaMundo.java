869080d82a77a3f261132ad030c79650c4139ab0
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 22:38:25 -0300

    Atualização

--- src/EXERCICIO_001_OlaMundo.java
+++ src/EXERCICIO_001_OlaMundo.java
@@ -1,4 +1,7 @@
 public class EXERCICIO_001_OlaMundo {
+
+    //Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
     }
