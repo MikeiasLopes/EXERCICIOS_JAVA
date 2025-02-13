7f8b63f1804bfa62b949813c497018287218d86f
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-12 22:48:55 -0300

    Atualização

--- src/EXERCICIO_006_AntecessorSucessor.java
+++ src/EXERCICIO_006_AntecessorSucessor.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class EXERCICIO_006_AntecessorSucessor {
     public static void main(String[] args) {
+
+        //Faça um programa em Java que leia um número inteiro e mostre o seu antecessor e seu sucessor.
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Digite um número: ");
         int num = sc.nextInt();
+
         int antecessor = num - 1;
         int sucessor = num + 1;
-        System.out.println("O antecessor de " +num+ " é: " +antecessor+ "\nO sucessor de " +num+ " é: " +sucessor);
 
+        System.out.println("O antecessor de " +num+ " é: " +antecessor+ "\nO sucessor de " +num+ " é: " +sucessor);
     }
 }
