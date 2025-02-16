96fcd93546f4fe555ce4b3a6554e4769839552ad
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-16 14:23:35 -0300

    Atualização

---
+++ src/EXERCICIO_002_PrintName.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EXERCICIO_002_PrintName {
+    public static void main(String[] args) {
+
+        // Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas
+        // vindas para ela:
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Olá " +nome+ ", é um prazer de conhecer!");
+    }
+}
---
+++ src/EXERCICIO_009_QuantoDinheiroCompraDolar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EXERCICIO_009_QuantoDinheiroCompraDolar {
+    public static void main(String[] args) {
+
+        //Desenvolva um programa em Java que leia uma distância em metros e mostre os
+        //valores relativos em outras medidas.
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
