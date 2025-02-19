4e20fda0d2e8d3310fbbfce40ddd5605d8382579
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-18 21:49:57 -0300

    Atualização

---
+++ src/EXERCICIO_012_PrecoPromocional.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EXERCICIO_012_PrecoPromocional {
+    public static void main (String[] args) {
+
+        // Crie um programa em Java que leia o preço
+        // de um produto, calcule e mostre o seu  PREÇO PROMOCIONAL,
+        // com 5% de desconto.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o valor do produto: ");
+        double preco = sc.nextDouble();
+
+        double promocao = preco - (preco * 0.05);
+
+        System.out.print("Preço promocional com 5% de desconto é R$ " +promocao);
+
+    }
+}
+
+// Informe o valor do produto: 100
+// Preço promocional com 5% de desconto é R$ 95.0
