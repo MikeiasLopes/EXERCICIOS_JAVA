4d19bbfb2154f3745f239cda612865ac96ff8d20
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-21 19:49:20 -0300

    Atualização

--- src/EXERCICIO_015_DiasTrabalhados.java
+++ src/EXERCICIO_015_DiasTrabalhados.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class EXERCICIO_015_DiasTrabalhados {
+    public static void main(String[] args) {
+
+        // Crie um programa Java que leia o número de dias trabalhados em
+        // um mês e mostre o  salário de um funcionário, sabendo que ele
+        // trabalha 8 horas por dia e ganha R$25 por  hora trabalhada.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o quantitativo de dias trabalhados: ");
+        double DiasTrabalhados = sc.nextDouble();
+
+        double salario = DiasTrabalhados * 200;
+        System.out.print("Seu salário do mês é R$ " + salario);
+    }
 }
+
+// Informe o quantitativo de dias trabalhados: 16
+// Seu salário do mês é R$ 3200.0
