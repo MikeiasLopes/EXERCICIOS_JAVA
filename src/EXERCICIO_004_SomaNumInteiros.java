cfdf28250f70727e68d618b12d2d1566618ac5f0
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-22 22:51:46 -0300

    Atualização

--- src/EXERCICIO_004_SomaNumInteiros.java
+++ src/EXERCICIO_004_SomaNumInteiros.java
@@ -7,14 +7,18 @@ public class EXERCICIO_004_SomaNumInteiros {
 
         Scanner sc = new Scanner(System.in);
 
+        // Solicita um número inteiro.
         System.out.print("Digite um número inteiro: ");
         int num1 = sc.nextInt();
 
+        // Solicita um segundo número inteiro.
         System.out.print("Digite um número inteiro: ");
         int num2 = sc.nextInt();
 
+        // Realiza a soma dos números inteiros digitados nos inputs.
         int soma = num1 + num2;
 
+        // Exibe a soma dos números inteiros.
         System.out.println("A soma entre " + num1 + " e " + num2 + " é igual a " + soma);
     }
 }
