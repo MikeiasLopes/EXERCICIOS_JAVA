2a5183409a49c06e5ade08c0f8a32f4dd9e244f1
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-22 23:14:18 -0300

    Atualização

--- src/EXERCICIO_010_LarguraAltura.java
+++ src/EXERCICIO_010_LarguraAltura.java
@@ -9,20 +9,31 @@ public class EXERCICIO_010_LarguraAltura {
 
         Scanner sc = new Scanner(System.in);
 
+        // Pede o comprimento da parede.
         System.out.print("Comprimento da parede em metro: ");
         double comprimento = sc.nextDouble();
 
+        // Pede a altura da parede.
         System.out.print("Altura da parede em metro: ");
         double altura = sc.nextDouble();
-        double area = altura * comprimento;
-        double tinta = altura * comprimento;
 
+        // Calcula a area que será pintada.
+        double area = altura * comprimento;
+
+        //Calcula a tinta necessária, 1 l pinta 2 m²).
+        double tinta = area / 2;
+
+
+        // Exibe o resultado.
         System.out.print("Area a ser pintada é " + area + " metros\nTinta necessária é de " + tinta / 2 + " litros");
+
+        // Fecha o scanner
+        sc.close();
 
     }
 }
 
-// Comprimento da parede: 25,6
-// Altura da parede: 23,1
-// Area a ser pintada é 591.36
-// Tinta necessária é de 295.68
+// Comprimento da parede em metro: 30
+// Altura da parede em metro: 3
+// Area a ser pintada é 90.0 metros
+// Tinta necessária é de 22.5 litros
