7990125cad76915fbc3a7c2748ebdd8ea26acf29
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-22 22:54:20 -0300

    Atualização

--- src/EXERCICIO_005_SomaNota.java
+++ src/EXERCICIO_005_SomaNota.java
@@ -8,12 +8,15 @@ public class EXERCICIO_005_SomaNota {
 
         Scanner sc = new Scanner(System.in);
 
+        // Solicita a primeira nota do aluno.
         System.out.print("Digite a nota do aluno(a): ");
         float nota1 = sc.nextFloat();
 
+        // Solicita a segunda nota do aluno.
         System.out.print("Digite a nota do aluno(a): ");
         float nota2 = sc.nextFloat();
 
+        // Exibe a média das notas dos inputs.
         float media = (nota1 + nota2) / 2;
 
         System.out.print("A média entre " +nota1+ " e " +nota2+ " é igual a " + media);
