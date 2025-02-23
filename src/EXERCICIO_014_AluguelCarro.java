15bba0500ca958c9be164e8d07197ebf1f3e1eaf
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-22 23:33:10 -0300

    Atualização

--- src/EXERCICIO_014_AluguelCarro.java
+++ src/EXERCICIO_014_AluguelCarro.java
@@ -11,27 +11,29 @@ public class EXERCICIO_014_AluguelCarro {
 
         Scanner sc = new Scanner(System.in);
 
+        double precoDia = 90; // Preço por dia alugado
+        double precoKm = 0.20;  // Preço por km rodado
+
+        // solicita a quantidade km rodados.
         System.out.print("Informe a quantidade de quilômetros percorridos: ");
         double km = sc.nextDouble();
 
+        // Solicita a quantidade de dia alugado.
         System.out.print("Informe a quantidade de dias que o carro foi alugado: ");
         double dias = sc.nextDouble();
 
-        System.out.print("Informe o valor por quilômetro rodado: ");
-        double PrecoKm = sc.nextDouble();
+        // Calcula o preço total.
+        double precoTotal = (precoDia * dias) + (precoKm * km );
 
-        System.out.print("Informe o valor por dia alugado: ");
-        double PrecoDia = sc.nextDouble();
+        // Exibe o resultado do preço final.
+        System.out.printf("Você percorreu %.0f km por %.0f dias, o preço final fica em R$ %.2f\n", km, dias, precoTotal);
 
-        double PrecoTotal = (PrecoDia * dias) + (PrecoKm * km );
-
-        System.out.printf("Você percorreu %.0f km por %.0f dias, o preço final fica em R$%.2f", km, dias, PrecoTotal);
+        // Fecha o scanner
+        sc.close();
 
     }
 }
 
-// Informe a quantidade de quilômetros percorridos: 1000
-// Informe a quantidade de dias que o carro foi alugado: 30
-// Informe o valor por quilômetro rodado: 0,20
-// Informe o valor por dia alugado: 90
-// Você percorreu 1000 km por 30 dias, o preço final fica em R$ 90006,00
+// Informe a quantidade de quilômetros percorridos: 300
+// Informe a quantidade de dias que o carro foi alugado: 5
+// Você percorreu 300 km por 5 dias, o preço final fica em R$ 510,00
