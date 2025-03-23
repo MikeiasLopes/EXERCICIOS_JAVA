1595c3571ed8bbc281b87611b222e7f05bf419fd
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-03-23 00:08:20 -0300

    Atualização

--- src/EXERCICIO_017_VelocidadeCarro.java
+++ src/EXERCICIO_017_VelocidadeCarro.java
@@ -10,17 +10,27 @@ public class EXERCICIO_017_VelocidadeCarro {
 
         Scanner sc = new Scanner(System.in);
 
+        //Usuário vai informar a velocidade do veículo.
         System.out.print("Velocidade do veículo em Km/h: ");
         double velocidade = sc.nextDouble();
 
+        //Vai verificar se a velocidade está acima de 80 km/h.
         if (velocidade > 80) {
+
+            //Excesso recebe a velocidade informada e subtrair por 80 (velocidade média).
             double excesso = velocidade - 80;
+            //Multa recebe o excesso e multiplica por 5 (valor multa po km).
             double multa = excesso * 5;
+            //Vai informar ao usuário caso tenha ultrapassado a velocidade máxima permitida.
             System.out.printf("Você foi multado! Valor da multa é R$ %.2f.", multa);
+
+        //Vai verificar se a velocidade está igual ou menor que a mínima permitida.
         } else {
+            //Vai informar ao usuário caso ele não tenha ultrapassado a velocidade máxima permitida.
             System.out.print("Velocidade no limite permitido.");
         }
 
+        //Fecha o scanner
         sc.close();
     }
 }
