3d7b9b84e228d8522a022bc93dfdc02b9f24cf27
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-03-23 00:32:12 -0300

    Atualização

--- src/EXERCICIO_021_AnoBissexto.java
+++ src/EXERCICIO_021_AnoBissexto.java
@@ -8,18 +8,24 @@ public class EXERCICIO_021_AnoBissexto {
 
         Scanner sc = new Scanner(System.in);
 
+        //Usuário vai digitar um ano qualquer.
         System.out.print("Digite um ano: ");
         int ano = sc.nextInt();
 
+        //Vai verificar se o ano informado, dividido por 4, terá com resultado o resto igual a 0.
         if (ano % 4 == 0) {
+            //Vai informar ao usuário que o ano é bissexto.
             System.out.print(ano + " é um ano bissexto!");
         }
 
+        //Vai verificar se o ano informado, dividido por 4, terá com resultado o resto diferente a 0.
         else {
+            //Vai informar ao usuário que o ano não é bissexto.
             System.out.print(ano + " não é um ano bissexto.");
 
         }
 
+        //Fecha o scanner.
         sc.close();
 
     }
